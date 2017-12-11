bf407f2385411451c3021b9ab7bdc453842f75a8
elzamin <elzamin006@mail.ru>
2017-12-11 19:18:23 +0300

    DZ v.0.089

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -20,47 +20,86 @@ public:
 	{
 		weight = 0;
 		POINT START = {};
-		way = &START;
+		head = &START;
+		*way = *head;
 	}
+
 	void add_(string *text) 
 	{
 		POINT A = {};
 		*A.date = *text;
 		A.weight = size(*text);
 		weight = weight + A.weight;
-
+		way->weight = way->weight + A.weight;
 		int h = 0;
+
 		do 
 		{
 			if (!way->left)
-			{
 				way->left = &A;
-			}
 			if (!way->right)
-			{
 				way->right = &A;
-			}
 			way->right;
 			h++;
 		} while (way->date != nullptr || !way->right);
 		*way = *head;
-		for (int i = 0; i < h + 1; i++)
-			head->right;
+
+		for (int i = 0; i < h; i++)
+		{
+			way->right;
+			way->weight = way->weight + A.weight;
+		}
+		way->right;
 		if (way->left != nullptr)
+		{
 			way->right = &A;
+			way->weight = way->weight + A.weight;
+		}
 		else
 		{
 			*way = *head;
 			for (int i = 0; i < h; i++)
-				head->right;
+				way->right;
 			POINT B = {};
 			*B.left = *way->right;
 			B.right = &A;
 			way->right = &B;
+			B.weight = B.left->weight + B.right->weight;
 		}
+		*way = *head;
 	}
+	
 	void delete_(int key) {}
-	void search_(int key) {}
+	
+	char search_(int key) 
+	{
+		POINT * secret;
+		char value;
+		do
+		{
+			*way = *secret;
+			if (way->left == nullptr && way->right == nullptr)
+			{
+				value = way->date[key];
+				return date[key];
+			}
+			else 
+			{
+				if (way->left != nullptr && way->right != nullptr)
+				{
+					way->left;
+					if (key >= way->weight)
+						secret->left;
+					else
+						secret->right;
+				}
+				if (way->right == nullptr)
+					secret->left;
+				if (way->left == nullptr)
+					secret->right;
+			}
+		} while (key == key);
+	}
 	void min_(int key) {}
 	void max_(int key) {}
 	void print_() {}
