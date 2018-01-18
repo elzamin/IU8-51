3ab4a46f96d48dc7aa145aafe8bb1f9e15bcc513
elzamin <elzamin006@mail.ru>
2018-01-18 12:49:57 +0300

    Update rope.h

--- Rope/rope.h
+++ Rope/rope.h
@@ -1,124 +1,190 @@
 #pragma once
-#include "Node.h"
 #include <string>
 
-class Rope
+
+class Node
 {
 private:
-	Node *root;
+	std::string *date;
+	int weight;
+	Node *left;
+	Node *right;
 public:
-	Rope()
-		:root(nullptr)
+	Node()
 	{
-		std::cout << "----------------<\nCreated new rope\n";
+		date = nullptr;
+		left = nullptr;
+		right = nullptr;
+		weight = 0;
 	};
 
-	Rope(std::string *a)
+	Node(std::string *a)
 	{
-		Node *newnode = new Node();
-		root = newnode;
-		root->date = a;
-		int wght = a->length();
-		newnode->weight = wght;
-		/*newnode->left = nullptr;
-		newnode->right = nullptr;*/
-		std::cout << "----------------<\nCreated new rope\n";
-		std::cout << "+\nAdded new string: \n<" << *(newnode->date) << ">\nWeight: " << newnode->date->length() << std::endl;
+		weight = a->length();
+		date = nullptr;
+		right = nullptr;
+		left = new Node();
+		left->date = a;
+		left->weight = a->length();
+		left->left = nullptr;
+		left->right = nullptr;
+		std::cout << "+\nAdded new string: <" << *(a) << "> Weight: " << a->length() << std::endl;
 	};
-	
-	/** 
+
+	/**
 	*	Добавление новой строки в дерево.
 	*	Новая строка добавляется правее предыдущих строк.
 	*	Консоль информирует о добавлении строки, её содежимом и весе.
 	*/
 	void Add(std::string *a)
 	{
-		Node *newnode = new Node();
-		Node *ptr;
-		Node *back;
-		int wght = a->length();;
-		newnode->date = a;
-		newnode->weight = wght;
-		if (!root)
-			root = newnode;
-		back = ptr = root;
-		while (!ptr->right || !ptr->left)
+		Node *ptr, *back;
+		Node *newone = new Node();
+
+		weight += a->length();
+		back = ptr = this;
+		newone->date = a;
+		newone->weight = a->length();
+		newone->left = nullptr;
+		newone->right = nullptr;
+		while (ptr->right && ptr->left)
+		{
 			back = ptr;
-			ptr->right;
-		if (ptr->date != nullptr)
+			ptr->right->weight += a->length();
+			ptr = ptr->right;
+
+		}
+		if (ptr->date)
 		{
 			Node *knot = new Node();
 			knot->left = ptr;
-			knot->right = newnode;
+			knot->right = newone;
 			knot->date = nullptr;
+			knot->weight += ptr->weight;
+			ptr->weight -= a->length();
 			back->right = knot;
-			back = ptr = nullptr;
 		}
 		else
 		{
-			if (!ptr->right && ptr->left != nullptr)
-				ptr->right = newnode;
+			if (!ptr->left)
+				ptr->left = newone;
 			else
-				ptr->left = newnode;
+				ptr->right = newone;
 		}
-		std::cout << "+\nAdded new string: \n<"<< *(newnode->date) << ">\nWeight: " <<newnode->date->length() << std::endl;
+		//ptr->weight += a->length();
+		back = ptr = nullptr;
+		std::cout << "+\nAdded new string: <" << *(a) << "> Weight: " << a->length() << std::endl;
 	}
 
 	/**
-	*
-	*
+	*	Вывод всех строк из дерева.
+	*	Вывод в консоль по порядку слева-направо.
 	*/
-	void Print() 
+
+	void PrintString()
 	{
-		if (!root)
-			std::cout << "Rope is empty\n";
-		else
+		std::cout << "\nResult:\nTotal weight: " << weight << std::endl;
+		if (!(this->date) && !(this->left) && !(this->right))
 		{
-			Node* a = root;
-
-			Printhelper(a);
-		}
-	};
-	void Printhelper(Node *node)
-	{
-		if (!node)
+			std::cout << "\nRope is empty" << std::endl;
 			return;
-		if (node->left != nullptr)
-			Printhelper(node->left);
-		if (node->right != nullptr)
-			Printhelper(node->right);
-		if (!node->date)
-			std::cout << '* ';
+		}
 		else
-			std::cout << *(node->date) << ' ';
+			Printhelper(this);
+		std::cout << std::endl << std::endl;
 	}
-	void Check()
-	{
-		std::cout << root->weight << *(root->date);
-		/*std::cout << root->left;
-		root->left = nullptr;
-		std::cout << root->left;
-		*/
-	}
-	/**
-	*	
-	*	
-	*/
 
-	~Rope()
+	void Printhelper(Node *temp)
 	{
-		//delete
+		if (temp == nullptr)
+		{
+			return;
+		}
+		if (temp->date)
+			std::cout << *(temp->date);
+		Printhelper(temp->left);
+		Printhelper(temp->right);
+
+	}
+
+	/**
+	*	Поиск строки из дерева.
+	*	Принимает значение key, далее через сравнение с weight находит и передаёт нужную строку.
+	*/
+	std::string SearchStr(int key)
+	{
+		Node *ptr = this;
+
+		if (key > weight)
+			return "error #1: key > weight";
+		while ((weight - ptr->right->weight) < key && (ptr->right->right != nullptr))
+			ptr = ptr->right;
+		if (weight - ptr->weight + ptr->left->weight <= key)
+			return *(ptr->right->date);
+		else
+			return *(ptr->left->date);
+	}
+
+	/**
+	*	Поиск символа из дерева.
+	*	Принимает значение key, далее через сравнение с weight находит и передаёт нужную строку.
+	*/
+	char SearchChar(int key)
+	{
+
+	}
+
+	/**
+	*	Удаление строки из дерева.
+	*	Принимает значение key, далее через сравнение с weight находит и удаляет нужную строку.
+	*	Консоль информирует об удалении строки и её содежимом.
+	*/
+	void DeleteOne(int key)
+	{
+		Node *ptr = this;
+		int i = 0, dweight;
+
+		if (key > weight)
+		{
+			std::cout << "error #1: key > weight" << std::endl;
+			return;
+		}
+		while ((weight - ptr->right->weight) < key && (ptr->right->right != nullptr))
+		{
+			ptr = ptr->right;
+			i++;
+		}
+		if (weight - ptr->weight + ptr->left->weight <= key)
+			dweight = ptr->right->weight;
+		else
+			dweight = ptr->left->weight;
+		ptr = this;
+		for (i; i != 0; i--)
+		{
+			ptr->weight -= dweight;
+			ptr = ptr->right;
+		}
+		ptr->weight -= dweight;
+		if (weight - ptr->weight + ptr->left->weight <= key)
+			ptr->right->DeleteHelper();
+		else
+			ptr->left->DeleteHelper();
+	}
+
+	void DeleteHelper()
+	{
+		std::cout << "-\nDeleted string: <" << *(date) << ">\n";
+		date = nullptr;
+		left = right = nullptr;
+		weight = 0;
+	}
+
+	~Node()
+	{
+		left->~Node();
+		right->~Node();
+		left = right = nullptr;
+		date = nullptr;
+		weight = 0;
 	}
 };
-
-void Print(Node *node)
-{
-	if (!node || node->itstr == 0)
-		return;
-	Print(node->left);
-	Print(node->right);
-	if (node->itstr == 0)
-		std::cout << '* ';
-	else
-		std::cout << *(node->date) << ' ';
-}
